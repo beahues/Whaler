bbcc16b9c2d9490897b201cb47358255f8d22e12
Bill Wang <bill.wang>
2023-06-30 17:07:25 +1000

    add in pipeline

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ platform="linux/arm/v7,linux/arm64/v8,linux/arm/v6,linux/amd64,linux/ppc64le,lin
 curl -H "Cache-Control: no-cache" -sL "https://raw.githubusercontent.com/alpine-docker/multi-arch-libs/stable/functions.sh" -o functions.sh
 source functions.sh
 
-tag="$(git rev-parse --short HEAD)"
+tag="$2"
 build_arg="VERSION=${tag}"
 
 echo "build image for ${tag}"
