c60393d07bd97d8a39a3b7220639f9bec1acc338
Bill Wang <bill.wang>
2023-07-02 21:47:00 +1000

    adjust platforms

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ Usage() {
 
 #pushd $1
 image="alpine/$1"
-platform="linux/arm/v7,linux/arm64/v8,linux/arm/v6,linux/amd64,linux/ppc64le,linux/s390x" 
+platform="linux/arm/v7,linux/arm64/v8,linux/amd64,linux/ppc64le,linux/s390x" 
 
 curl -H "Cache-Control: no-cache" -sL "https://raw.githubusercontent.com/alpine-docker/multi-arch-libs/stable/functions.sh" -o functions.sh
 source functions.sh
