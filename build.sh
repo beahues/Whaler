c9e64c0a9af9e52fa17bb5527b197c48cf3f3580
Bill Wang <bill.wang>
2023-07-02 21:12:48 +1000

    update build

--- build.sh
+++ build.sh
@@ -19,9 +19,8 @@ platform="linux/arm/v7,linux/arm64/v8,linux/arm/v6,linux/amd64,linux/ppc64le,lin
 curl -H "Cache-Control: no-cache" -sL "https://raw.githubusercontent.com/alpine-docker/multi-arch-libs/stable/functions.sh" -o functions.sh
 source functions.sh
 
-tag="$2"
-build_arg="VERSION=${tag}"
+tag="latest"
+build_arg="VERSION-${tag}"
 
 echo "build image for ${tag}"
-docker buildx rm -f mybuilder || echo "ok"
 build_docker_image "${tag}" "${image}" "${platform}" "${build_arg}"
